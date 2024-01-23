880f701c40abfe819bf12f881088f450147441ed
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-01-23 18:09:21 -0400

    Update Dashboard Layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,8 +5,10 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
-        <div className="flex flex-col relative w-screen">
-            <div id="menu" className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 fixed left-0 h-screen overflow-y-scroll">
+        
+        <div className="flex">
+            
+            <div id="menu" className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
               <div id="logo" className="my-4 px-6">
                 <h1 className="text-lg md:text-2xl font-bold text-white">Dash<span className="text-blue-500">8</span>.</h1>
                 <p className="text-slate-500 text-sm">Manage your actions and activities</p>
@@ -103,6 +105,11 @@ export default function DashboardLayout({
                 </a>
               </div>
             </div>
+
+            <div className="p-2 w-full text-slate-900">
+              { children }
+            </div>
+
         </div>
     </div>
   );
