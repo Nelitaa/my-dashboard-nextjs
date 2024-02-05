2124b2f4f3e37902a753a061189315734fc6f05a
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-02-05 13:58:05 -0400

    Add function to fetch Pokemon data from the API

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,8 +1,21 @@
+const getPokemons = async ( limit = 20, offset = 0) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+  const data = await response.json();
+  return data;
+}
 
-export default function PokemonsPage() {
+export default async function PokemonsPage() {
+
+  const pokemons = await getPokemons();
+
   return (
     <div>
       <h1>Hello Pokemons</h1>
+      <ul>
+        {pokemons.results.map(pokemon => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
