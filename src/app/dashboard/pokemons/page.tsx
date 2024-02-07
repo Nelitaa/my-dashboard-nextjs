626ed6ff106d946f9d5b8c9a65a6a103c5a71f78
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-02-07 15:28:39 -0400

    Show all pokemons

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -19,12 +19,17 @@ export default async function PokemonsPage() {
   return (
     <div className="flex flex-col">
       <div className="flex flex-wrap gap-10 items-center justify-center">
-        <Image
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/10.svg`}
-          width={100}
-          height={100}
-          alt="Pokemon name"
-        />
+        {
+          pokemons.map(pokemon => (
+            <Image
+            key={ pokemon.id }
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ pokemon.id }.svg`}
+            width={100}
+            height={100}
+            alt={ pokemon.name }
+          />
+          ))
+        }
       </div>
     </div>
   );
