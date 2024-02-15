acb821cfb7e7b85bcd557f071fa9b9f52653e157
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-02-14 22:06:39 -0400

    Add Error Page

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -8,6 +8,9 @@ const getPokemons = async ( limit = 20, offset = 0 ):Promise<SimplePokemon[]> =>
     id: pokemon.url.split('/').at(-2)!,
     name: pokemon.name,
   }));
+
+  // throw new Error('This error should not happen');
+
   return pokemons;
 }
 
