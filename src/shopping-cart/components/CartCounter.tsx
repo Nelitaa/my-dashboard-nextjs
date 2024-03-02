df902ef42d3a9a77f4f5ef95669827c236acb07f
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-03-01 23:46:07 -0400

    Add actions to counter slice

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,26 +1,31 @@
 'use client';
 
+import { useAppSelector } from '@/store';
+import { addOne, substractOne } from '@/store/counter/counterSlice';
 import React from 'react'
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 interface Props {
   value?: number;
 }
 
 export const CartCounter = ({ value = 0 }: Props) => {
-  const [count, setCount] = useState(value);
+  const count = useAppSelector( state => state.counter.count );
+  const dispatch = useDispatch();
+  // const [count, setCount] = useState(value);
 
   return (
     <>
       <span className="text-9xl"> { count } </span>
       <div className="flex">
       <button 
-        onClick={() => setCount(count + 1)}
+        onClick={() => dispatch( addOne() )}
         className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
         +1
       </button>
       <button 
-        onClick={() => setCount(count - 1)}
+        onClick={() => dispatch( substractOne() )}
         className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
         -1
       </button>
