c13131d7ee2a5357fe7ebea7c3f9d3594bae6147
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-03-03 23:39:05 -0400

    Make HTTP call to endpoint to fetch count value

--- src/components/dashboard/WidgetsGrid.tsx
+++ src/components/dashboard/WidgetsGrid.tsx
@@ -14,6 +14,7 @@ export const WidgetsGrid = () => {
       subTitle="Products in cart"
       label="Counter"
       icon={ <IoCartOutline size={ 70 } className="text-blue-600" />}
+      href="/dashboard/counter"
     />
   </div>
   )
