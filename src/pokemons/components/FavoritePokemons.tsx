b809131b60f4ab7e1d2bd84406b1449057f1f059
Marianela Munoz Gutierrez <marianelamunozgutierrez@gmail.com>
2024-03-06 22:29:13 -0400

    Add function for no favorites

--- src/pokemons/components/FavoritePokemons.tsx
+++ src/pokemons/components/FavoritePokemons.tsx
@@ -3,12 +3,29 @@
 import { useState } from "react";
 import { useAppSelector } from "@/store";
 import { PokemonGrid } from "./PokemonGrid";
+import { IoHeartOutline } from "react-icons/io5";
 
 export const FavoritePokemons = () => {
   const favoritePokemons = useAppSelector(state => Object.values( state.pokemons ) );
   const[pokemons, setPokemons] = useState( favoritePokemons);
 
   return (
-    <PokemonGrid pokemons={ pokemons } />
+    <>
+      {
+        pokemons.length === 0
+          ?(<NoFavorites />)
+          :(<PokemonGrid pokemons={ pokemons } />)
+      }
+    </>
+  )
+}
+
+
+export const NoFavorites = () => {
+  return (
+    <div className="flex flex-col h-[50vh] items-center justify-center">
+      <IoHeartOutline size={100} className="text-red-500" />
+      <span>No favorites yet</span>
+    </div>
   )
 }
